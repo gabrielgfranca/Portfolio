8b4be9df201764344dbb542d392ad7861ca1e93a
Gabriel Franca <gabrielgfranca7@gmail.com>
2025-07-09 15:46:30 -0300

    Marquee Effect - Not Finished

--- lib/Sections/devtools_section.dart
+++ lib/Sections/devtools_section.dart
@@ -1,9 +1,29 @@
 import 'package:flutter/material.dart';
 import '../build_section.dart';
 import '../constants.dart';
+import 'package:marquee_widget/marquee_widget.dart';
 
-class DevtoolsSection extends StatelessWidget {
+class DevtoolsSection extends StatefulWidget {
   const DevtoolsSection({super.key});
+
+  @override
+  State<DevtoolsSection> createState() => _DevtoolsSectionState();
+}
+
+class _DevtoolsSectionState extends State<DevtoolsSection> {
+  final logos = [flutterLogo, dartLogo, kotlinLogo, pythonLogo, cLogo];
+  List<Widget> repeatWidgets(List<Widget> list, int times) {
+    return List.filled(times, list)
+        .expand(
+          (e) => e.map(
+            (item) => Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 18.0),
+              child: item,
+            ),
+          ),
+        )
+        .toList();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -11,10 +31,45 @@ class DevtoolsSection extends StatelessWidget {
       flex: 1,
       color: kSecondarySectionPrimaryColor,
       child: [
-        const Text(
-          'Section 2 - Part 1',
-          style: TextStyle(color: Colors.white, fontSize: 24.0),
-          textAlign: TextAlign.center,
+        Expanded(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Expanded(
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Container(
+                    constraints: const BoxConstraints(minHeight: 200.0),
+                    decoration: BoxDecoration(
+                      color: kSecondarySectionSecondaryColor,
+                      borderRadius: BorderRadius.circular(10.0),
+                    ),
+                    child: Row(
+                      children: [
+                        Expanded(
+                          child: Marquee(
+                            direction: Axis.horizontal,
+                            directionMarguee: DirectionMarguee.oneDirection,
+                            autoRepeat: true,
+                            animationDuration: const Duration(seconds: 15),
+                            backDuration: Duration.zero,
+                            pauseDuration: Duration.zero,
+                            child: SizedBox(
+                              width: 1000,
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: repeatWidgets(logos, 2),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ],
     );
--- lib/constants.dart
+++ lib/constants.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
 const kCornerRadius = 20.0;
 const kPadding = 20.0;
+const kLogoSize = 60.0;
 
 // Main Section Colors
 const kMainSectionPrimaryColor = Color(0xff2A4BB3);
@@ -17,3 +19,34 @@ const kSecondarySectionTertiaryColor = Color(0xff101016);
 const kSecondarySectionQuaternaryColor = Color(0xff948979);
 const kSecondarySectionQuinaryColor = Color(0xff686259);
 const kSecondarySectionSenaryColor = Color(0xffECB236);
+
+ColorFilter logoColor = ColorFilter.mode(
+  Colors.white.withValues(alpha: 0.75),
+  BlendMode.srcIn,
+);
+// Logo Widgets
+final Widget flutterLogo = SvgPicture.asset(
+  'icons/flutter-plain.svg',
+  height: kLogoSize,
+  colorFilter: logoColor,
+);
+final Widget dartLogo = SvgPicture.asset(
+  'icons/dart-plain.svg',
+  height: kLogoSize,
+  colorFilter: logoColor,
+);
+final Widget kotlinLogo = SvgPicture.asset(
+  'icons/kotlin-plain.svg',
+  height: kLogoSize,
+  colorFilter: logoColor,
+);
+final Widget pythonLogo = SvgPicture.asset(
+  'icons/python-plain.svg',
+  height: kLogoSize,
+  colorFilter: logoColor,
+);
+final Widget cLogo = SvgPicture.asset(
+  'icons/c-plain.svg',
+  height: kLogoSize,
+  colorFilter: logoColor,
+);
